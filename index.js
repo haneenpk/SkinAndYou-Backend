f0575cccbf46705fc672a344986905169c21f30b
Muhammed Haneen <mohdhaneenpk666@gmail.com>
2024-10-09 01:47:46 +0530

    modify index file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose")
 const noCache = require('nocache');
 const cors = require('cors')
 
-mongoose.connect('mongodb+srv://mohdhaneenpk666:mP6YS3N4sh8dXhFC@skintask.5ttgv.mongodb.net/?retryWrites=true&w=majority&appName=skintask')
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected successfully"))
     .catch((err) => console.log(err))
 
